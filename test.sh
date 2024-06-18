2bbd49b6e84bce08404de7a15fced86dc8097f67
yanagitani <takanori.yanagitani@gmail.com>
2024-06-19 08:15:22 +0900

    test: simpler script

--- test.sh
+++ test.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
 
-features(){
-    echo simple
-
-    echo simple_red
-    echo simple_red_wasm
-
-    echo simple_gray
-    echo simple_gray_wasm
-}
-
 cargo \
-    test \
-    --features $( features | tr '\n' , | sed 's/,$//' )
+	test \
+	--all-features
